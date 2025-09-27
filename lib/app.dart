b706bf8e15b6a298c89a1319f5a3051db840d0dd
jay-isampelliwar <isampelliwar79@gmail.com>
2025-09-27 10:26:52 +0530

    Refactor app structure: integrate Supabase initialization, implement MultiBlocProvider for authentication, and update MyHomePage layout with a new Scaffold and AppBar.

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ifindlife_app/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'core/theme/theme.dart';
+import 'features/auth/presentation/cubit/authentication_cubit.dart';
+import 'injection/injection.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'I Find Life',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      title: 'iFindLife',
+      theme: ITheme.lightTheme,
+      darkTheme: ITheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthenticationCubit>(
+            create: (context) => getIt<AuthenticationCubit>(),
+          ),
+        ],
+        child: LoginPage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  // Get a reference your Supabase client
+  final supabase = Supabase.instance.client;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("I Find Live")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "I Find Live",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:ifindlife_app/app.dart';
+import 'package:ifindlife_app/injection/injection.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://dqgxhlgksomohrxwhwic.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZ3hobGdrc29tb2hyeHdod2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNjM5MDMsImV4cCI6MjA3MDkzOTkwM30.-EK4yNOuU4qZ2HwWypMyyvemKiG8e5RGKQZszIFyAgs',
+  );
+
+  // Initialize dependency injection
+  await init();
+
+  runApp(MyApp());
 }
